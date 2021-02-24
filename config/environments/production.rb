806cbb84c997b205bc443761fba9c3f0e67d5269
Ubuntu <ubuntu@ip-172-31-16-20.ap-northeast-1.compute.internal>
2021-02-24 10:55:37 +0000

    fixed line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = https://rails-tutorial-ryogo.herokuapp.com/
+  host = "https://rails-tutorial-ryogo.herokuapp.com"
   
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
